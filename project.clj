badf1f4bd46409b9d0dc261f8a17261cd0356040
luke <nerorevenge@gmail.com>
2016-11-22 01:42:47 -0500

    Added core.matrix for vector calculations

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-		 [org.apache.spark/spark-core_2.11 "2.0.1"]
-                 [yieldbot/flambo "0.8.0"]]
+                 [org.apache.spark/spark-core_2.11 "2.0.1"]
+                 [yieldbot/flambo "0.8.0"]
+                 [net.mikera/core.matrix "0.57.0"]]
   :aot [flambo.function])
